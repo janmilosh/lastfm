a38681903309779a3d20df08ddd912a7a1f46edb
Jan Milosh <janmilosh@gmail.com>
2014-09-06 09:04:52 -0400

    Adds events model and initial migration.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=200)
+    timestamp = models.DateTimeField('timestamp')
+
+    def __unicode__(self):
+        return self.location
+
+    class Meta:
+        ordering = ('timestamp',)
