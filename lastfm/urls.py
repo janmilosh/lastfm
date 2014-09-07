b1a53dfa98fd3d858675d02628dbc146a5c98be0
Jan Milosh <janmilosh@gmail.com>
2014-09-06 23:56:21 -0400

    Adds login, logout, and registration.

--- lastfm/urls.py
+++ lastfm/urls.py
@@ -4,6 +4,13 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'lastfm.views.login'),
+    url(r'^auth/$', 'lastfm.views.auth_view'),
+    url(r'^logout/$', 'lastfm.views.logout'),
+    url(r'^loggedin/$', 'lastfm.views.loggedin'),
+    url(r'^invalid/$', 'lastfm.views.invalid_login'),
+    url(r'^register/$', 'lastfm.views.register_user'),
+    url(r'^register_success/$', 'lastfm.views.register_success'),
     url(r'^$', 'lastfm.views.home', name='home'),
     url(r'^events/', include('events.urls')),
 )
--- lastfm/views.py
+++ lastfm/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import logout
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, render_to_response
+from django.contrib import auth
+from django.contrib.auth import logout, login, authenticate 
+from django.core.context_processors import csrf
+from forms import MyRegistrationForm
 
 from events.models import Location
-
 
 def home(request):
     locations = Location.objects.all()
@@ -10,3 +13,48 @@ def home(request):
     return render(request, "home.html", ({
         'locations': locations
     }))
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render(request, 'account/login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return redirect('/')
+    else:
+        return redirect('/invalid/')
+
+def loggedin(request):
+    return render(request, 'account/loggedin.html', ({
+        'full_name': request.user.username
+    }))
+
+def invalid_login(request):
+    return render(request, 'account/invalid-login.html')
+
+def logout(request):
+    auth.logout(request)
+    return render(request, 'account/logout.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = MyRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/register_success/')
+
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = MyRegistrationForm()
+
+    return render(request, 'account/register.html', args)
+
+def register_success(request):
+    return render(request, 'account/register-success.html')
